dddb5b6e924151f227ffc8c2520f0d4eb1d179e9
kai <kai.gib@tuta.io>
2023-07-11 21:02:29 +0300

    added systray on top of alpha

--- config.def.h
+++ config.def.h
@@ -6,6 +6,12 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
+
 static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
@@ -36,6 +42,7 @@ static char tagsselbgcolor[]             = "#c0d5a0";
 
 static const unsigned int baralpha = 251;
 static const unsigned int borderalpha = OPAQUE;
+
 static const char *colors[][3]      = {
     /*    fg                  bg                  border        */
     { normfgcolor,        normbgcolor,        normbordercolor    },  /* SchemeNorm */
